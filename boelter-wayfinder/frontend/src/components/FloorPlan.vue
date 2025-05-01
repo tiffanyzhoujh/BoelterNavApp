5743a4a6879f4b79db340bce39e71d3d27b185fa
tiffanyzhoujh <tiffanyzhoujh@outlook.com>
2025-05-01 11:09:23 -0700

    deployment fix

--- boelter-wayfinder/frontend/src/components/FloorPlan.vue
+++ boelter-wayfinder/frontend/src/components/FloorPlan.vue
@@ -75,7 +75,7 @@ async function fetchPath() {
   if (!startDot || !destDot || startDot === destDot) return
 
 
-  const res = await axios.post('http://192.168.50.18:5000/api/path', {
+  const res = await axios.post('https://boelterwayfinderbackend.onrender.com/api/path', {
     start: startDot,
     dest: destDot
   }, { responseType: 'blob' }) 
